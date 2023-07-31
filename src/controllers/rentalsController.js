3240b8c10ade67571ff3a8a4e581e17428130ba2
Willyan <wkinhaamaral@gmail.com>
2023-07-31 08:28:18 -0300

    feat: delete:rentalsbyid

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -91,17 +91,16 @@ export async function postrentalsreturn(req, res) {
 }
 
 export async function delrentals(req, res) {
-    const { name, phone, cpf, birthday } = req.body
+   
     const { id } = req.params
-    console.log(cpf)
+   
     try {
-        const customers = await db.query(`SELECT * FROM customers WHERE id=$1`, [id]);
+        const del = await db.query(`SELECT * FROM rentals WHERE id=$1`, [id]);
 
-        if (customers.rows.length === 0) return res.sendStatus(404)
-        const checkcpf = await db.query(`SELECT * FROM customers WHERE cpf=$1 AND id<>$2 `, [cpf, id]);
-        if (checkcpf.rows.length > 0) return res.sendStatus(409)
-
-        const creatgames = await db.query(`UPDATE customers SET  name=$1, phone=$2, cpf=$3, birthday=$4 WHERE id=$5`, [name, phone, cpf, birthday, id]);
+        if (del.rows.length === 0) return res.sendStatus(404)
+        if (del.rows[0].returnDate === null) return res.sendStatus(400)
+        console.log('deleted')
+       await db.query(`DELET FROM rentals WHERE id=$1`, [id]);
         res.sendStatus(200);
     } catch (err) {
         res.status(500).send(err.message);
--- src/routes/rentalsRoutes.js
+++ src/routes/rentalsRoutes.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 
-import { schemascustomers } from "../schemas/customersSchemas.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import { delrentals, getrentals, postrentalsinit, postrentalsreturn } from "../controllers/rentalsController.js";
+import { schemarentals } from "../schemas/rentalsSchemas.js";
 
 
 const rentalsRouter = Router()
 
 rentalsRouter.get("/rentals", getrentals);
-rentalsRouter.post("/rentals", postrentalsinit);
+rentalsRouter.post("/rentals", validateSchema(schemarentals), postrentalsinit);
 //rentalsRouter.post("/rentals/:id/return", validateSchema(schemascustomers), postrentalsreturn);
-//rentalsRouter.delete("/rentals/:id", validateSchema(schemascustomers), delrentals);
+rentalsRouter.delete("/rentals/:id", delrentals);
 
 
 export default rentalsRouter
